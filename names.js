df22c084656d8f8d0c2f32186ed080b3a86b3be4
Joel Tsuchitori <TsuchitoriJoel@gmail.com>
2019-01-31 10:05:58 -1000

    added file to keep track of separate names

---
+++ names.js
@@ -0,0 +1,12 @@
+function findNames(name){
+  nameList.forEach(function{child}{
+      if(child.name == name){
+        return child.names;
+      }
+  });
+}
+
+var nameList  = [
+  {name:"", names:["",""]},
+
+]
